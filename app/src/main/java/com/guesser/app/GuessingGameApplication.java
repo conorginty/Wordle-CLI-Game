b6c1c7b3cb260708816caccf50df471b301a1034
Conor Ginty <conorginty@gmail.com>
2022-03-07 21:55:28 +0000

    Begin modularising the application code

--- app/src/main/java/com/guesser/app/GuessingGameApplication.java
+++ app/src/main/java/com/guesser/app/GuessingGameApplication.java
@@ -1,4 +1,4 @@
-package com.guesser.guessinggame;
+package com.guesser.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
