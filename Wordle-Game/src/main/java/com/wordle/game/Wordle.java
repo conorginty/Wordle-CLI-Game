7059c7e6d3eb61a372346d82588cc953f757ffbb
Conor Ginty <conorginty@gmail.com>
2022-03-11 21:30:38 +0000

    Add logic to get random word to be guessed

--- Wordle-Game/src/main/java/com/wordle/game/Wordle.java
+++ Wordle-Game/src/main/java/com/wordle/game/Wordle.java
@@ -1,7 +1,19 @@
 package com.wordle.game;
 
+import com.guesser.api.getter.LocalWordGetter;
+import com.guesser.api.getter.WordGetter;
+
 public class Wordle {
     public void playGame() {
         System.out.println("You are playing Wordle!");
+        String actualWord = getRandomFiveLetterWordInUpperCase();
+        System.out.println(actualWord);
     }
+
+    public String getRandomFiveLetterWordInUpperCase() {
+        WordGetter wordGetter = new LocalWordGetter();
+        String randomWord = wordGetter.getRandomWordOfLengthN(5).getWord();
+        return randomWord.toUpperCase();
+    }
+
 }
